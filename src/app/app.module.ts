150d61ae82d10817a8157a44b2ad53c5c1a21a86
marcelo <marc.x.antunes@gamil.com>
2024-07-16 08:49:39 -0300

    Revert "Projeto5"
    This reverts commit e9e6d7396ecc41d857f92fb4b0cf0269baabcb15.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,18 +14,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RestritoRoutingModule } from './restrito/restrito-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { RestritoComponent } from './restrito/restrito.component';
-import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
-import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
-import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
-import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -33,12 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
     InicioComponent,
     LoginComponent,
     MenuComponent,
-    RodapeComponent,
-    RestritoComponent,
-    AtualizaProdutoComponent,
-    CadastroProdutoComponent,
-    ListaProdutoComponent,
-    MenuRestritoComponent
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
@@ -51,11 +38,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatToolbarModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RestritoRoutingModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
