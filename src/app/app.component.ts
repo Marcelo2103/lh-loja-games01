8efd83ebfea5f765f2b0d07e77c4939f950a81c0
marcelo <marc.x.antunes@gamil.com>
2024-07-16 16:49:11 -0300

    Projeto5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { LoginService } from './login.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'LH Games';
-    mostrarMenu:boolean = true;
-
-    constructor (private _loginService: LoginService){}
-
-    ngOnInit(): void {
-      this._loginService.getMostraMenu().subscribe(
-        res => {this.mostrarMenu = res; }
-      )
-    }
-
-    ngOnDestroy(){
-      localStorage.clear();
-    }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { LoginService } from '../login.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,17 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
-  usuario! : string;
-  senha! : string;
-
-  constructor (private _loginService:LoginService,private _router:Router){}
-
-  fazerLogin(){
-
-    this._loginService.login(this.usuario, this.senha);
-    this._router.navigate(['/restrito/lista']);
-    this._loginService.setMostraMenu(false);
-  }
 
 }
